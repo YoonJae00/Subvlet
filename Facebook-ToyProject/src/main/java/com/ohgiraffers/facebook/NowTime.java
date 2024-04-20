e965fc09c372e116b273e44bca8df468e73baaed
yoonjae <realyoon77@gmail.com>
2024-04-20 22:39:10 +0900

    복습

--- Facebook-ToyProject/src/main/java/com/ohgiraffers/facebook/NowTime.java
+++ Facebook-ToyProject/src/main/java/com/ohgiraffers/facebook/NowTime.java
@@ -13,6 +13,6 @@ public class NowTime extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute();
+        req.getParameter("");
     }
 }
